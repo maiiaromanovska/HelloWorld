0f51eca04f02ec21994166d666e3024f7246fd22
Maya Romanovska <maiiaromanovska@gmail.com>
2023-07-10 13:38:28 +0200

    changes for issue SP3828

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Git and Maya!");
-        System.out.println("Hello QA Manual!");
+       
         // one more change
 
     }    
