edb1d3bacccbee4dbacf500399cfd06f36ff8488
Maya Romanovska <maiiaromanovska@gmail.com>
2023-07-10 13:42:59 +0200

    changes from Maya Romanovska

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
         System.out.println("Hello Git and Maya!");
        
         // one more change
+		System.out.println("Hello World! I am Maya Romanovska!");
 
     }    
 }
